7f3c466fb5bfccde40bb0307845c95ee0f4a8af8
Mentor <mentor@palokaj.co>
2025-09-25 15:51:34 +0200

    🔥 remove firebase boilerplate

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-import { v2_oncall } from './runtime/on_call_runtimes.mjs'
-
-
-export const do_a_thing = v2_oncall( async () => {
-    return 'did a thing'
-} )
--- src/hooks/user.js
+++
@@ -1,2 +0,0 @@
-// Re-export the optimized useUser hook from the Zustand store
-export { useUser } from '../stores/user'
